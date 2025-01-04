704554447deaaf54a3d7382300ffa78cca649158
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2025-01-03 20:28:06 -0600

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repeatText, repeatNum) {
 
+    let result = "";
+
+    if (repeatNum < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < repeatNum; i++) {
+        result += repeatText;
+    }
+    return result;
 };
 
 // Do not edit below this line
