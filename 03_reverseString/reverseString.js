3955fe771874157acff110020edda9945f666ca8
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2025-01-03 20:46:07 -0600

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputText) {
 
+    const stringArr = inputText.split("");
+    const reveresedArr = [];
+
+    for (let i = stringArr.length - 1; i >= 0; i--) {
+
+        reveresedArr.push(stringArr[i]);
+
+    }
+
+    return reveresedArr.join("");
 };
 
 // Do not edit below this line
