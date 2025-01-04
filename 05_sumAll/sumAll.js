615bbf82346088ca2e9aafcae8f2c093a77db535
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2025-01-04 14:36:39 -0600

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (typeof start !== "number" || typeof end !== "number") {return "ERROR";}
+    
+    if (start < 0 || end < 0) {return "ERROR";}
+
+    if (Math.floor(start) !== start || Math.floor(end) !== end) {return "ERROR";}
+
+    let sum = 0;
+    let i, j;
+
+    if (start > end) {
+        i = end;
+        j = start;
+    } else {
+        i = start;
+        j = end;
+    }
+
+    for (; i <= j; i++) {
+
+        sum = sum + i;
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
