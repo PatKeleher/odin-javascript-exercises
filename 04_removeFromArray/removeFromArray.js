be33414205ed174f28ecc1d49cb65ce91dbf4fc5
Pat Keleher <122421655+PatKeleher@users.noreply.github.com>
2025-01-04 13:53:41 -0600

    Exercise 4 complete; could've used more built-in javascript array methods to simplify our approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argList) {
+
+    const filteredArray = array.filter(notEqual);
+
+    function notEqual(value) {
+
+        let result = true;
+
+        for (const parameter of argList) {
+            if (value === parameter) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
